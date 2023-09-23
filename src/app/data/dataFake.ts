12764c3de3eb0960d1b54aca19f7bedf28714dc9
Odair-Loretti <odairgabriel1995@gmail.com>
2023-09-22 21:30:35 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"NOVO VINGADORES ANUNCIADO",
+        "description":"Marvel estudios anuncia novo Vingadores, 4 anos após ultimato.",
+        "photoCover":"https://internacionaldaamazoniacom.files.wordpress.com/2021/12/capa-1.jpg"
+
+    },
+    {
+        "id":"2",
+        "title":"NOVO FILME DOS GUARDIÕES DA GALÁXIA",
+        "description":"ola",
+        "photoCover":"https://s2-techtudo.glbimg.com/E-v8xnunDY0WI5ZbVKR3U5FrxO8=/0x0:1068x655/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/x/A/W4CpeFTUuqJy2ERdGmPQ/guardioes-da-galaxia-3-1068x655.jpg"
+    }
+]
