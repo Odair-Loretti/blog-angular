28caa7e561604809869aa16611ac6463b4238900
Odair-Loretti <odairgabriel1995@gmail.com>
2023-09-22 21:42:36 -0300

    content add

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,14 +2,27 @@ export const dataFake = [
     {
         "id":"1",
         "title":"NOVO VINGADORES ANUNCIADO",
-        "description":"Marvel estudios anuncia novo Vingadores, 4 anos após ultimato.",
+        "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id orci auctor quam pretium imperdiet. Mauris aliquam lacinia tincidunt. Maecenas velit metus, venenatis a ultricies in, sodales sed dolor. Integer posuere commodo eros, quis auctor justo aliquam a. Cras molestie libero et malesuada auctor. Nulla porta luctus urna, id congue erat ultrices vitae. Phasellus tempus finibus commodo. Maecenas turpis nisl, dapibus eget ullamcorper accumsan, vestibulum vitae libero. Duis pulvinar felis at est varius lobortis. Pellentesque ullamcorper elit leo, vitae ornare diam facilisis ac.",
         "photoCover":"https://internacionaldaamazoniacom.files.wordpress.com/2021/12/capa-1.jpg"
 
     },
     {
         "id":"2",
         "title":"NOVO FILME DOS GUARDIÕES DA GALÁXIA",
-        "description":"ola",
+        "description":"ToDonec id gravida lectus. Suspendisse feugiat leo non quam interdum, sed lacinia nibh ornare. In dictum dolor eu cursus porttitor. Proin ornare dolor sed dignissim scelerisque. Nullam nisi nulla, fermentum at facilisis at, lobortis commodo ligula. Nullam gravida quam auctor leo tempor euismod. Cras commodo neque a eros hendrerit ultricies. Sed eros ligula, iaculis sed sapien quis, egestas tristique quam. Aenean est neque, commodo eget pretium a, facilisis eu libero. Proin tempus felis at nibh auctor, sit amet ultrices sem volutpat. Mauris eu sapien neque. Morbi porttitor, urna vitae pulvinar tempus, tellus libero tristique justo, non maximus orci lorem ac elit",
         "photoCover":"https://s2-techtudo.glbimg.com/E-v8xnunDY0WI5ZbVKR3U5FrxO8=/0x0:1068x655/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/x/A/W4CpeFTUuqJy2ERdGmPQ/guardioes-da-galaxia-3-1068x655.jpg"
+    },
+    {
+        "id":"3",
+        "title":"NOVO AQUAMAN SERÁ LANÇADO EM BREVE",
+        "description":"Curabitur cursus at magna sed pharetra. Sed odio nulla, rhoncus a nibh eu, fringilla pretium ex. Aliquam pharetra, eros eu dictum fringilla, mauris libero sollicitudin lectus, sit amet accumsan dui erat at elit. Proin eget ex nisi. Vivamus vulputate mollis est sit amet auctor. Etiam sit amet pharetra purus. Sed scelerisque, mauris in congue porttitor, mi sem dapibus sapien, nec fringilla libero ipsum et magna.",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBjgn89y6Hc6SBhDz9ovIrYALI5RH7BR-lLw&usqp=CAU"
+    },
+    {
+        "id":"4",
+        "title":"FILME DO BESOURO AZUL FLOPA NA BILHETERIA",
+        "description":"Proin faucibus augue vulputate, scelerisque felis in, rutrum sem. Phasellus sed augue et augue sagittis efficitur. Aenean consectetur dui sem, quis malesuada ligula rutrum nec. In justo ex, congue in libero sed, mollis fermentum nisl. Proin augue felis, ultricies ut quam vitae, mattis blandit lacus. Curabitur facilisis magna lacus, ut vehicula velit blandit sit amet. Vestibulum consequat euismod consequat. Nullam a congue nulla. Suspendisse potenti. Etiam gravida mauris a nunc euismod, ac bibendum mi mollis. Aenean eget lorem sed lectus interdum bibendum. Aliquam vitae lobortis arcu.",
+        "photoCover":"https://cinepop.com.br/wp-content/uploads/2023/08/besouro-azul-4-696x391.jpg"
     }
+
 ]
